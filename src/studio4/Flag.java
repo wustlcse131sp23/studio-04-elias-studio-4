07dcc57330873227900c1ce91ce617a529520eb5
EliasBarro <barro@wustl.edu>
2023-02-23 12:46:20 -0600

    flag

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -6,6 +6,18 @@ import edu.princeton.cs.introcs.StdDraw;
 
 public class Flag {
 	public static void main(String[] args) {
-		
+		StdDraw.setPenColor(0,0,0);
+		StdDraw.filledRectangle(.5, .5, .3, .2);
+		StdDraw.setPenColor(231,178,198);
+		StdDraw.filledCircle(.465,.5,.0465);
+		StdDraw.filledCircle(.535,.5,.0465);
+		double [] x = {.415,.585,.5};
+		double [] y = {.5,.5,.35};
+		StdDraw.filledPolygon(x, y);
+		StdDraw.setPenRadius(.01);
+		StdDraw.setPenColor(255,0,0);
+		StdDraw.arc(.5,.5,.2,000,180);
+		StdDraw.setPenColor(255,150,0);
+		StdDraw.arc(.5,.5,.19,000,180);
+		}
 	}
-}
